a457c61cec005587ec091a25b9231025711a444d
baasit <basitmohammed362@gmail.com>
2023-11-16 19:52:02 +0000

    hello

--- hey5.c
+++ hey5.c
@@ -9,44 +9,44 @@
 */
 int display(char *line_c, stack_t **stack, unsigned int counter, FILE *file)
 {
-  instruction_t opst[] = {
-        {"push", pushing_p}, {"pall", lapoid}, {"pint", f_pint},
-        {"pop", pop_up},
-        {"swap", swap},
-        {"add", add_up},
-        {"nop", f_nop},
-        {"sub", subtract},
-        {"div", divide},
-        {"mul", multiply},
-        {"mod", modulos},
-        {"pchar", chars},
-        {"pstr", point_string},
-        {"rotl", rotme},
-        {"rotr", devfest},
-        {"queue", queue},
-        {"stack", stack_them},
-        {NULL, NULL}
-        };
-  unsigned int x = 0;
-  char *op;
+instruction_t opst[] = {
+{"push", pushing_p}, {"pall", lapoid}, {"pint", f_pint},
+{"pop", pop_up},
+{"swap", swap},
+{"add", add_up},
+{"nop", f_nop},
+{"sub", subtract},
+{"div", divide},
+{"mul", multiply},
+{"mod", modulos},
+{"pchar", chars},
+{"pstr", point_string},
+{"rotl", rotme},
+{"rotr", devfest},
+{"queue", queue},
+{"stack", stack_them},
+{NULL, NULL}
+};
+unsigned int x = 0;
+char *op;
 
-  op = strtok(line_c, " \n\t");
-  if (op && op[0] == '#')
-    return (0);
-  bus.arg = strtok(NULL, " \n\t");
-  while (opst[x].opcode && op)
-  {
-    if (strcmp(op, opst[x].opcode) == 0)
-    {  opst[x].f(stack, counter);
-      return (0);
-    }
-    x++;
-  }
-  if (op && opst[x].opcode == NULL)
-  { fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
-    fclose(file);
-    free(line_c);
-    stack_free(*stack);
-    exit(EXIT_FAILURE); }
-  return (1);
+op = strtok(line_c, " \n\t");
+if (op && op[0] == '#')
+return (0);
+bus.arg = strtok(NULL, " \n\t");
+while (opst[x].opcode && op)
+{
+if (strcmp(op, opst[x].opcode) == 0)
+{  opst[x].f(stack, counter);
+return (0);
+}
+x++;
+}
+if (op && opst[x].opcode == NULL)
+{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+fclose(file);
+free(line_c);
+stack_free(*stack);
+exit(EXIT_FAILURE); }
+return (1);
 }
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 #include "levels.h"
 #include <stdio.h>
 
@@ -12,36 +10,36 @@ defo bus = {NULL, NULL, NULL, 0};
 */
 int main(int argc, char *argv[])
 {
-    char *line_c = NULL;
-    FILE *file;
-    size_t size = 0;
-    ssize_t read_line = 1;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+char *line_c = NULL;
+FILE *file;
+size_t size = 0;
+ssize_t read_line = 1;
+stack_t *stack = NULL;
+unsigned int counter = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    while (read_line > 0)
-    {
-        line_c = NULL;
-        read_line = getline(&line_c, &size, file);
-        counter++;
-        if (read_line > 0)
-        {
-            display(line_c, &stack, counter, file);
-        }
-        free(line_c);
-    }
-    stack_free(stack);
-    fclose(file);
-    return (0);
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+file = fopen(argv[1], "r");
+if (!file)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+while (read_line > 0)
+{
+line_c = NULL;
+read_line = getline(&line_c, &size, file);
+counter++;
+if (read_line > 0)
+{
+display(line_c, &stack, counter, file);
+}
+free(line_c);
+}
+stack_free(stack);
+fclose(file);
+return (0);
 }
